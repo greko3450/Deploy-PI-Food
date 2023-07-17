919c2ff58adfa6a02c23e805e937b83b8455f2e5
greko3450 <johannsm03@gmail.com>
2023-07-17 14:37:33 -0500

    action

--- client/src/components/pages/Landing.jsx
+++ client/src/components/pages/Landing.jsx
@@ -11,7 +11,7 @@ class Landing extends React.Component {
           <button className={landingPage.boton}>Home</button>
         </NavLink>
         <div className={landingPage.content}>
-          <h1>*************PI food forrrrrrrrrrrrrrrrr*************</h1>
+          <h1>*************PI food*************</h1>
           <p>"Hoy quiero presentarte un proyecto relacionado con la página web de Spoonacular, enfocado en la implementación de tecnología de PI. Spoonacular es una página web dedicada a la gastronomía que ofrece una amplia variedad de recetas de cocina, artículos relacionados con alimentos y bebidas, y herramientas útiles para planificar y organizar comidas. La página web cuenta con una gran base de datos de recetas, pero una de las dificultades que enfrenta es la selección y sugerencia de recetas personalizadas para cada usuario en particular.
 
 El objetivo principal de este proyecto es mejorar la experiencia del usuario al buscar y seleccionar recetas personalizadas, utilizando tecnología de PI. Esto significa que la página web será capaz de analizar y comprender el lenguaje natural utilizado por los usuarios en sus búsquedas, y proporcionar resultados precisos y relevantes de acuerdo con sus preferencias y necesidades.
